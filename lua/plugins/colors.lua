066bc948ae7943c38a312f36c60c3c3049cd6e8a
vlad cojocaru <vladvcojocaru@gmail.com>
2025-01-04 17:19:19 +0200

    dont remember

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -11,29 +11,45 @@ return {
     },
     -- Other plugins here...
 
+    -- {
+    --     'navarasu/onedark.nvim',
+    --     config = function()
+    --         require('onedark').setup {
+    --             -- Main options --
+    --             style = 'darker',     -- Change this to your preferred style
+    --             transparent = false,  -- Set background transparency
+    --             term_colors = true,   -- Match terminal colors
+    --             ending_tildes = true, -- Show tildes at the end of buffers
+    --             code_style = {
+    --                 comments = 'italic',
+    --                 keywords = 'bold',
+    --                 functions = 'italic',
+    --                 strings = 'none',
+    --                 variables = 'none',
+    --             },
+    --             diagnostics = {
+    --                 darker = true,
+    --                 undercurl = true,
+    --                 background = true,
+    --             },
+    --         }
+    --         require('onedark').load()
+    --     end
+    -- },
     {
-        'navarasu/onedark.nvim',
+        "olimorris/onedarkpro.nvim",
+        priority = 1000, -- Ensure it loads first
+        init = function()
+     --       vim.cmd("colorscheme onedark")
+        end
+
+
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
         config = function()
-            require('onedark').setup {
-                -- Main options --
-                style = 'darker',     -- Change this to your preferred style
-                transparent = false,  -- Set background transparency
-                term_colors = true,   -- Match terminal colors
-                ending_tildes = true, -- Show tildes at the end of buffers
-                code_style = {
-                    comments = 'italic',
-                    keywords = 'bold',
-                    functions = 'italic',
-                    strings = 'none',
-                    variables = 'none',
-                },
-                diagnostics = {
-                    darker = true,
-                    undercurl = true,
-                    background = true,
-                },
-            }
-            require('onedark').load()
+            vim.cmd("colorscheme rose-pine-moon")
         end
     }
 }
